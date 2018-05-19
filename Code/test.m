54ef22850b2a75b292707f3eedd3c9f12dbdeffe
Chan-wei <huchanwei1204@gmail.com>
2018-05-20 01:54:28 +0800

    Add files via upload

--- Code/test.m
+++ Code/test.m
@@ -22,7 +22,7 @@ quoted_edges = quoted_price_lb:1:quoted_price_ub;
 supply_action_num = size(quoted_edges,2);
 
 % Discretize the supply state space => demand 
-demand_ub = 50;
+demand_ub = 200;
 demand_lb = 0;
 demand_state_edges = demand_lb:1:demand_ub;
 demand_state_num = size(demand_state_edges,2) - 1;
@@ -35,7 +35,7 @@ buy_edges = buy_price_lb:1:buy_price_ub;
 user_action_num = size(buy_edges,2);
 
 % Discretize the demand state space => supply
-supply_ub = 50;
+supply_ub = 200;
 supply_lb = 0;
 supply_state_edges = supply_lb:1:supply_ub;
 supply_state_num = size(supply_state_edges,2) - 1;
@@ -62,12 +62,13 @@ Result = struct('sup_benefit_RL', [], 'sup_benefit_Random', [], ...
 for day = 1:day_num
     plants_data = squeeze(plants_data_base(day, :, :));
     % NOTE: should change power_dem shape if you have multiple users
-    power_dem = squeeze(total_power_dem(day,:,:))';
+    power_dem = squeeze(total_power_dem(day, :, :));
+    fprintf('==> Testing day: %d\n', day);
     for use_RL = 0:1
         for compute_time = 7:18
             if use_RL 
                 % Get supplier current state
-                sup_cur_state = discretize(power_dem(compute_time, :), demand_state_edges)*ones(plant_num);
+                sup_cur_state = discretize(plants_data(compute_time, :), demand_state_edges);
                 for i = 1:plant_num
                     % Get the index of the max Q-value in current state, that is
                     % the price to quote
@@ -77,31 +78,30 @@ for day = 1:day_num
                 % Get user current state
                 usr_cur_state = discretize(power_dem(compute_time, :), supply_state_edges);
                 for i = 1:buy_num
-                    [~, buy_price(i)] = max(usr_Q_factor(i, usr_cur_state, :)); 
+                    [~, buy_price(i)] = max(usr_Q_factor(i, usr_cur_state(i), :)); 
                     buy_price(i) = buy_price(i)+1;
                 end
             else
-                quoted_price = discretize(rand(1, plant_num)*7+2, quoted_edges);
-                buy_price = discretize(rand(1, buy_num)*5+2, buy_edges);
+                quoted_price = randi([quoted_price_lb quoted_price_ub], 1, plant_num);
+                buy_price = randi([buy_price_lb buy_price_ub], 1, buy_num);
             end
             f = [-1 * buy_price quoted_price];
             b = [power_dem(compute_time, :) plants_data(compute_time, :)];
 
-            [x, fval, exitflag, output] = linprog(f, A, b, Aeq, beq, zeros(1, plant_num + buy_num), 1000 * ones(1, plant_num + buy_num));
-            fval_log = [fval_log -fval];
-            meanPrice = mean(x(2:plant_num + buy_num ));
-            eve_x_log = [eve_x_log meanPrice];
+            [x, fval, exitflag, output] = linprog(f, A, b, Aeq, beq, ...
+                zeros(1, plant_num + buy_num), 1000 * ones(1, plant_num + buy_num), [], ...
+                optimset('Display','none'));
             if use_RL
-                Result.sup_benefit_RL = [Result.sup_benefit_RL sum(quoted_price*x(2:plant_num+buy_num))];              
-                usr_demand_rate = x(1:buy_num)/power_dem(compute_time, :);
+                Result.sup_benefit_RL = [Result.sup_benefit_RL sum(quoted_price*x(buy_num+1:plant_num+buy_num))];              
+                usr_demand_rate = x(1:buy_num)'/power_dem(compute_time, :);
                 Result.usr_benefit_RL = [Result.usr_benefit_RL sum(usr_demand_rate*(1-buy_price/buy_price_ub))];
-                Result.actual_supply_RL = [Result.actual_supply_RL sum(x(2:plant_num+buy_num))];
+                Result.actual_supply_RL = [Result.actual_supply_RL sum(x(buy_num+1:plant_num+buy_num))];
             else
-                Result.sup_benefit_Random = [Result.sup_benefit_Random sum(quoted_price*x(2:plant_num+buy_num))];
-                usr_demand_rate = x(1:buy_num)/power_dem(compute_time, :);
+                Result.sup_benefit_Random = [Result.sup_benefit_Random sum(quoted_price*x(buy_num+1:plant_num+buy_num))];
+                usr_demand_rate = x(1:buy_num)'/power_dem(compute_time, :);
                 Result.usr_benefit_Random = [Result.usr_benefit_Random sum(usr_demand_rate*(1-buy_price/buy_price_ub))];
-                Result.actual_supply_Random = [Result.actual_supply_Random sum(x(2:plant_num+buy_num))];
-                Result.demand = [Result.demand power_dem(compute_time, :)];
+                Result.actual_supply_Random = [Result.actual_supply_Random sum(x(buy_num+1:plant_num+buy_num))];
+                Result.demand = [Result.demand sum(power_dem(compute_time, :))];
             end
         end
         if use_RL
--- Code/train.m
+++ Code/train.m
@@ -23,7 +23,7 @@ quoted_edges = quoted_price_lb:1:quoted_price_ub;
 supply_action_num = size(quoted_edges,2);
 
 % Discretize the supply state space => demand 
-demand_ub = 50;
+demand_ub = 200;
 demand_lb = 0;
 demand_state_edges = demand_lb:1:demand_ub;
 demand_state_num = size(demand_state_edges,2) - 1;
@@ -36,7 +36,7 @@ buy_edges = buy_price_lb:1:buy_price_ub;
 user_action_num = size(buy_edges,2);
 
 % Discretize the demand state space => supply
-supply_ub = 50;
+supply_ub = 200;
 supply_lb = 0;
 supply_state_edges = supply_lb:1:supply_ub;
 supply_state_num = size(supply_state_edges,2) - 1;
@@ -52,35 +52,43 @@ A = eye(plant_num+buy_num);
 Aeq = [ones(1 ,buy_num) -1*ones(1, plant_num)];
 beq = 0;
 
+% Maintain a table for weighting
+sup_weight = ones(plant_num, demand_state_num, supply_action_num);
+usr_weight = ones(buy_num, supply_state_num, user_action_num);
+
 % Start training
 for day = 1:day_num
     plants_data = squeeze(plants_data_base(day,:,:));
     % NOTE: should change power_dem shape if you have multiple users
-    power_dem = squeeze(total_power_dem(day,:,:))';
+    power_dem = squeeze(total_power_dem(day,:,:));
+    fprintf('==> Training day: %d\n', day);
     for compute_time = 7:18
         % Convert current demand to discrete state
-        dem_cur_state = discretize(power_dem(compute_time, :), demand_state_edges)*ones(plant_num,1);
-        %sup_cur_state = discretize(sum(plants_data(compute_time, :)), supply_state_edges);
+        %dem_cur_state = discretize(sum(power_dem(compute_time, :)), demand_state_edges)*ones(plant_num, 1);
+        dem_cur_state = discretize(plants_data(compute_time, :), demand_state_edges);
         sup_cur_state = discretize(power_dem(compute_time, :), supply_state_edges);
-        for iter_ = 1:iteration
+        for iter_ = 1:iteration            
             % Exploration
             quoted_price = randi([quoted_price_lb quoted_price_ub], 1, plant_num);
             buy_price = randi([buy_price_lb buy_price_ub], 1, buy_num);
-           
+            
             % Start evaluating 
             f = [-1 * buy_price quoted_price];
             b = [power_dem(compute_time, :) plants_data(compute_time, :)];
             [x, fval, exitflag, output] = linprog(f, A, b, Aeq, beq, ...
-                zeros(1, plant_num + buy_num), 1000 * ones(1, plant_num + buy_num));
+                zeros(1, plant_num + buy_num), 1000 * ones(1, plant_num + buy_num), [], ...
+                optimset('Display','none'));
             
             % Update the reward of supplier
             for j = 1:plant_num    
-                immi_reward = (x(j+1)/(plants_data(compute_time, j)+0.00001))*(quoted_price(j)/quoted_price_ub);
+                immi_reward = (x(j+buy_num)/(plants_data(compute_time, j)+0.00001))*(quoted_price(j)/quoted_price_ub);
                 % Get quoted price -> action num
                 quoted_p = quoted_price(j) - 1;
                 % Update supply Q-factor
                 sup_Q_factor(j,dem_cur_state(j),quoted_p) = (1-learning_rate)*sup_Q_factor(j, dem_cur_state(j), quoted_p)+ ...
                     learning_rate*(immi_reward + eta*(max(sup_Q_factor(j,dem_cur_state(j),:))));
+                % update weight matrix
+                sup_weight(j, dem_cur_state(j), quoted_p) = sup_weight(j, dem_cur_state(j), quoted_p) + 1;
             end
             % Update the reward of user
             for j = 1:buy_num
@@ -88,13 +96,17 @@ for day = 1:day_num
                 % Get quoted price -> action num
                 buy_p = buy_price(j) - 1;
                 % Update user Q-factor
-                usr_Q_factor(j,sup_cur_state,buy_p) = (1-learning_rate)*usr_Q_factor(j, sup_cur_state,buy_p)+ ...
-                    learning_rate*(immi_reward+ eta*(max(usr_Q_factor(j,sup_cur_state,:))));
+                usr_Q_factor(j,sup_cur_state(j),buy_p) = (1-learning_rate)*usr_Q_factor(j, sup_cur_state(j),buy_p)+ ...
+                    learning_rate*(immi_reward+ eta*(max(usr_Q_factor(j,sup_cur_state(j),:))));
+                % update weight matrix
+                usr_weight(j, sup_cur_state(j), buy_p) = usr_weight(j, sup_cur_state(j), buy_p) + 1;
             end
         end
     end
 end
 
 % Save the model
+sup_Q_factor = sup_Q_factor ./ sup_weight;
+usr_Q_factor = usr_Q_factor ./ usr_weight;
 save('sup_Q_factor.mat', 'sup_Q_factor');
 save('usr_Q_factor.mat', 'usr_Q_factor');
